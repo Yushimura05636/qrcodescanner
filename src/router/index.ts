668d777097e481e20e9fb33809ed4e6cdeffa7ab
Sasas <69789935+Yushimura05636@users.noreply.github.com>
2025-04-02 16:04:07 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -74,5 +74,6 @@ export default router
 export const config = {
   //API_URL: 'http://localhost:8000/api'
   //API_URL: 'https://qrscannerdb-production.up.railway.app/api'
-  API_URL: 'https://virtualliftoff.com/qrscanner/public/api'
+ //API_URL: 'https://virtualliftoff.com/qrscanner/public/api'
+  API_URL: 'https://laravelqrscanner.virtualliftoff.com/public/api'
 }
